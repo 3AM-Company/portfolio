f5a6aaac660d3370a05dd5fd1f1fb2206630e735
Mahmood Qudah <qudahmahmood@gmail.com>
2025-09-29 20:25:09 +0300

    fix gp

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/portfolio/',
+  base: command === 'build' ? '/portfolio/' : '/',
   server: {
     port: 3000,
     open: true
   }
-})
+}))
